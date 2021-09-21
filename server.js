573a797f1af550bc06ca82967078c70c9951d991
ShivamWho <shivam2094.be20@chitkara.edu.in>
2021-09-21 11:38:48 +0530

    add procfile and update ports

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ app.post("/", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
-  console.log("Server up at 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server started");
 });
